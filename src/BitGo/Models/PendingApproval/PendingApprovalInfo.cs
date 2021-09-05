3dbfcbb27e3fe265373dc726d535762f5dc03ad7
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-09-05 23:53:04 +0300

    -update field names;

--- src/BitGo/Models/PendingApproval/PendingApprovalInfo.cs
+++ src/BitGo/Models/PendingApproval/PendingApprovalInfo.cs
@@ -7,52 +7,53 @@ namespace MyJetWallet.BitGo.Models.PendingApproval
     [DataContract]
     public class PendingApprovalInfo
     {
-        [JsonProperty("id")]
+        [JsonProperty("id"), DataMember(Order = 1)]
         public string Id { get; internal set; }
         
-        [JsonProperty("coin")]
+        [JsonProperty("coin"), DataMember(Order = 2)]
         public string Coin { get; internal set; }
         
-        [JsonProperty("enterprise")]
+        [JsonProperty("enterprise"), DataMember(Order = 3)]
         public string Enterprise { get; internal set; }
         
-        [JsonProperty("wallet")]
+        [JsonProperty("wallet"), DataMember(Order = 4)]
         public string WalletId { get; internal set; }
         
-        [JsonProperty("walletLabel")]
+        [JsonProperty("walletLabel"), DataMember(Order = 5)]
         public string WalletLabel { get; internal set; }
         
-        [JsonProperty("creator")]
+        [JsonProperty("creator"), DataMember(Order = 6)]
         public string Creator { get; internal set; }
         
-        [JsonProperty("createDate")]
+        [JsonProperty("createDate"), DataMember(Order = 7)]
         public DateTime CreateDate { get; internal set; }
         
-        [JsonProperty("info")]
+        [JsonProperty("info"), DataMember(Order = 8)]
         public Info Info { get; internal set; }
         
         /// <summary>
         /// Enum:"pending" "approved" "rejected"
         /// </summary>
-        [JsonProperty("state")]
+        [JsonProperty("state"), DataMember(Order = 9)]
         public string State { get; internal set; }
         
-        [JsonProperty("userIds")]
+        [JsonProperty("userIds"), DataMember(Order = 10)]
         public string[] WalletUserIds { get; internal set; }
         
         /// <summary>
         /// number >= 1
         /// </summary>
-        [JsonProperty("approvalsRequired")]
+        [JsonProperty("approvalsRequired"), DataMember(Order = 11)]
         public int ApprovalsRequired { get; internal set; }
     }
 
+    [DataContract]
     public class Info
     {
-        [JsonProperty("type")]
+        [JsonProperty("type"), DataMember(Order = 1)]
         public string Type { get; internal set; }
         
-        [JsonProperty("transactionRequest")]
+        [JsonProperty("transactionRequest"), DataMember(Order = 2)]
         public TransactionRequest TransactionRequest { get; internal set; }
         
         [JsonProperty("updateEnterpriseRequest")]
@@ -62,25 +63,27 @@ namespace MyJetWallet.BitGo.Models.PendingApproval
         public UpdateApprovalsRequiredRequest UpdateApprovalsRequiredRequest { get; internal set; }
     }
 
+    [DataContract]
     public class TransactionRequest
     {
-        [JsonProperty("comment")]
+        [JsonProperty("comment"), DataMember(Order = 1)]
         public string Comment { get; internal set; }
-        [JsonProperty("requestedAmount")]
+        [JsonProperty("requestedAmount"), DataMember(Order = 2)]
         public string RequestedAmount { get; internal set; }
-        [JsonProperty("sourceWallet")]
+        [JsonProperty("sourceWallet"), DataMember(Order = 3)]
         public string SourceWallet { get; internal set; }
-        [JsonProperty("recipients")]
+        [JsonProperty("recipients"), DataMember(Order = 4)]
         public TransactionRecipient[] Recipients { get; internal set; }
     }
 
+    [DataContract]
     public class TransactionRecipient
     {
-        [JsonProperty("address")]
+        [JsonProperty("address"), DataMember(Order = 1)]
         public string Address { get; internal set; }
-        [JsonProperty("amount")]
+        [JsonProperty("amount"), DataMember(Order = 2)]
         public string Amount { get; internal set; }
-        [JsonProperty("data")]
+        [JsonProperty("data"), DataMember(Order = 3)]
         public string Data { get; internal set; }
     }
 
