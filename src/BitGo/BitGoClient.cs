837e9f701c5b0611f7cddc15d58b31b9e5f8a862
alexey <alexey.n@smplt.net>
2021-09-23 10:43:46 +0300

    add setting throwIfErrorResponse

--- src/BitGo/BitGoClient.cs
+++ src/BitGo/BitGoClient.cs
@@ -2,21 +2,34 @@ namespace MyJetWallet.BitGo
 {
     public class BitGoClient : IBitGoClient
     {
+        private BitGoApi _main;
+        private BitGoApi _test;
+        
         public BitGoClient(string accessTokenMainnet, string accessTokenTestnet)
         {
-            MainNet = new BitGoApi(accessTokenMainnet, BitGoNetwork.Main);
-            TestNet = new BitGoApi(accessTokenTestnet, BitGoNetwork.Test);
+            _main = new BitGoApi(accessTokenMainnet, BitGoNetwork.Main);
+            _test = new BitGoApi(accessTokenTestnet, BitGoNetwork.Test);
         }
         
         public BitGoClient(
             string accessTokenMainnet, string apiRootUrlMainnet,
             string accessTokenTestnet, string apiRootUrlTestnet)
         {
-            MainNet = new BitGoApi(accessTokenMainnet, apiRootUrlMainnet);
-            TestNet = new BitGoApi(accessTokenTestnet, apiRootUrlTestnet);
+            _main = new BitGoApi(accessTokenMainnet, apiRootUrlMainnet);
+            _test = new BitGoApi(accessTokenTestnet, apiRootUrlTestnet);
         }
         
-        public IBitGoApi MainNet { get; }
-        public IBitGoApi TestNet { get; }
+        public IBitGoApi MainNet => _main;
+        public IBitGoApi TestNet => _test;
+
+        public bool ThrowIfErrorResponse
+        {
+            get => _main.ThrowThenErrorResponse;
+            set
+            {
+                _main.ThrowThenErrorResponse = value;
+                _test.ThrowThenErrorResponse = value;
+            }
+        }
     }
 }
